6c5695bf6d8e9d6fe6edc5d4e291d0469ef0e0df
Mohammad Mohammadpour <116471351+shawshanck@users.noreply.github.com>
2023-06-12 12:11:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Unspalash API
 const count = 10;
-const apiKey = '24Rt3Iv73vLdNBVTlfoxFFYut5FwYseedQ-b3B8MCMs'
+const apiKey = 'API_KEY'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Get Photos from Unsplash API
